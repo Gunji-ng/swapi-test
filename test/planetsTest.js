e319bc3cd14e4e5cbf617e65b2cef196a2183526
Gunji <justgunji@gmail.com>
2022-02-23 12:03:37 +0100

    add negative test

--- test/planetsTest.js
+++ test/planetsTest.js
@@ -4,19 +4,13 @@ var chaiHttp = require('chai-http');
 chai.use(chaiHttp)
 
 const expect = chai.expect;
-//
-assert = chai.assert;
 
 describe('Planets endpoint tests', () => {
   describe('Perform regular tests', () => {
     it('Verify response details', (done) => {
-      let startTime = Date.now();
       chai.request('https://swapi.dev')
       .get('/api/planets/3/')
       .end((error, response) => {
-        let endTime = Date.now();
-        timeElapsed = endTime - startTime;
-
         expect(error).to.be.null;
         expect(response).to.have.status(200);
         expect(response).to.be.json;
@@ -42,6 +36,22 @@ describe('Planets endpoint tests', () => {
 
         expect(error).to.be.null;
         expect(timeElapsed).to.be.at.most(3);
+        done();
+      });
+    });
+  });
+
+  describe('Perform negative test', () => {
+    it('Post should return a 405 error', (done) =>  {
+      chai.request('https://swapi.dev')
+      .post('/api/planets/3/')
+      .send({'name': 'Automated testing', 'Completed': true})
+      .end((error, response) => {
+        let expectedResponse = 'Method \'POST\' not allowed.';
+
+        expect(error).to.be.null;
+        expect(response).to.have.status(405);
+        expect(response.body.detail).to.equal(expectedResponse);
         done();
       });
     });
