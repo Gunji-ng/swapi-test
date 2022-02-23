29f74968a1af68b8a0de6b32f26c4c8fb03d6cf2
Gunji <justgunji@gmail.com>
2022-02-23 12:36:37 +0100

    add schema verification test

--- test/planetsTest.js
+++ test/planetsTest.js
@@ -1,9 +1,95 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
+chai.use(require('chai-json-schema-ajv'));
 
 const expect = chai.expect;
+
+const planetSchema = {
+  title: 'Planets Schema',
+  type: 'object',
+  required: ['name',
+    'rotation_period',
+    'orbital_period',
+    'diameter',
+    'climate',
+    'gravity',
+    'terrain',
+    'surface_water',
+    'population',
+    'residents',
+    'films',
+    'created',
+    'edited',
+    'url'],
+  properties: {
+    'name': {
+      type: 'string',
+      minLength: 2
+    },
+    'rotation_period': {
+      type: 'string',
+      minLength: 1
+    },
+    'orbital_period': {
+      type: 'string',
+      minLength: 2
+    },
+    'diameter': {
+      type: 'string',
+      minLength: 1
+    },
+    'climate': {
+      type: 'string',
+      minLength: 3
+    },
+    'gravity': {
+      type: 'string',
+      minLength: 1
+    },
+    'terrain': {
+      type: 'string',
+      minLength: 3
+    },
+    'surface_water': {
+      type: 'string',
+      minLength: 1
+    },
+    'population':{
+      type: 'string',
+      minLength: 1
+    },
+    'residents':{
+      type: 'array',
+      uniqueItems: true,
+      minItems: 0,
+      items: {
+        type: 'string'
+      }
+    },
+    'films':{
+      type: 'array',
+      uniqueItems: true,
+      minItems: 0,
+      items: {
+        type: 'string'
+      }
+    },
+    'created':{
+      type: 'string',
+      minLength: 25
+    },
+    'edited':{
+      type: 'string',
+      minLength: 25
+    },
+    'url': {
+      type: 'string',
+      minLength: 20
+    }
+  }
+}
 
 describe('Planets endpoint tests', () => {
   describe('Perform regular tests', () => {
@@ -17,6 +103,7 @@ describe('Planets endpoint tests', () => {
         expect(response).to.have.header('content-type', 'application/json');
 
         let responseBody = response.body;
+        expect(responseBody).to.be.jsonSchema(planetSchema);
         expect(responseBody.name).to.be.equal('Yavin IV');
         expect(responseBody.diameter).to.be.equal('10200');
         expect(responseBody.population).to.be.equal('1000');
